e8c5f651e232538629c9f84d1c898cacb1f5f6d6
Seán O'Brien <sean95ob@gmail.com>
2024-03-11 21:05:12 +0000

    Update allowed hosts

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -71,7 +71,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-seanobrien95-drfapi-vpqqp4zhbuj.ws-eu108.gitpod.io', 'drf-api11.herokuapp.com']
+ALLOWED_HOSTS = ['8000-seanobrien95-drfapi-vpqqp4zhbuj.ws-eu108.gitpod.io', 'https://drf-api11-bdfdb88f585f.herokuapp.com']
 
 
 # Application definition
